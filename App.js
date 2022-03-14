bcf7cd6762c12d991ffdb6f403434d86a7f49b43
ADMIN <bgj443@myy.haaga-helia.fi>
2022-03-14 20:55:17 +0200

    Tehty

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Contacts from "expo-contacts";
+import React, { useState, useEffect } from "react";
+import {FlatList, StyleSheet, Text, View, Pressable  } from "react-native";
 
 export default function App() {
+  const [contact, setContact] = useState([]);
+
+  const getContacts = async () => {
+    const { status } = await Contacts.requestPermissionsAsync();
+    if (status === "granted") {
+      const { data } = await Contacts.getContactsAsync({
+        fields: [Contacts.Fields.PhoneNumbers],
+      });
+      if (data.length > 0) {
+        console.log(data);
+        setContact(data);
+      }
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        style={{ marginLeft: "5%" }}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.name}</Text>
+            <Text>{item.phoneNumbers[0].number}</Text>
+          </View>
+        )}
+        data={contact}
+      />
+      <Pressable style={styles.button} title="Get contacts" onPress={getContacts}>
+      <Text style={styles.text}>Get Contacts</Text>
+      </Pressable>
     </View>
   );
 }
@@ -13,8 +40,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 70,
+  },
+  button: {
     alignItems: 'center',
     justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 25,
+    borderRadius: 6,
+    elevation: 6,
+    backgroundColor: 'darkblue',
+    marginBottom: 30,
+  },
+  text: {
+    fontSize: 15,
+    lineHeight: 20,
+    fontWeight: 'bold',
+    letterSpacing: 0.30,
+    color: 'white',
   },
 });
